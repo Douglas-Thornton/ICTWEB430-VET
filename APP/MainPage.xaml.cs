5b542fe0136cfe4d1edcb972c6f3f002757d1a5a
Douglas Thornton <douglas.thornton@chamonix.com.au>
2023-08-29 14:54:27 +0930

    Updates to main page

--- APP/MainPage.xaml.cs
+++ APP/MainPage.xaml.cs
@@ -4,12 +4,19 @@ using System.ComponentModel;
 
 namespace APP
 {
+    /// <summary>
+    /// XAML main page, contains navigation links to other xaml pages and blazor webviews.
+    /// </summary>
     public partial class MainPage : INotifyPropertyChanged
     {
 
         new public event PropertyChangedEventHandler PropertyChanged;
         private readonly UserPreferences _userPreferences;
 
+        /// <summary>
+        /// Constructor for the main page.
+        /// </summary>
+        /// <param name="userPreferences"></param>
         public MainPage(UserPreferences userPreferences)
         {
 
@@ -23,11 +30,22 @@ namespace APP
             BindingContext = this;
         }
 
+        /// <summary>
+        /// If properties change invoke this.
+        /// </summary>
+        /// <param name="propertyName"></param>
         protected override void OnPropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
+        /// <summary>
+        /// When user preferences change invoke this.
+        /// Intended to update the font size and font family of xaml tabbed page headers.
+        /// Does not seem to be possible as of now.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void OnUserPreferencesChange(object sender, EventArgs e)
         {
             // Maui doesn't have controls for font-size and font for tabbed page?
