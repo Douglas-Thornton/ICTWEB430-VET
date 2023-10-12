abdcae78c05d6ce346a5be41ec1474163f172e5c
Elliott Ortmann <89680048+Elliott127@users.noreply.github.com>
2023-10-12 13:11:29 +1030

    new method to get all pets in pet service

--- APP/Services/Pet/PetService.cs
+++ APP/Services/Pet/PetService.cs
@@ -1,12 +1,36 @@
 ﻿using APP.Data.Models;
 using APP.Interfaces;
+using Newtonsoft.Json;
 
 namespace APP.Services;
 
 public class PetService : IPetService
 {
-    public Task<List<Pet>> GetAllPets()
+    private readonly string _baseUrl = "https://localhost:7020/";
+    public async Task<List<Pet>> GetAllPets()
     {
-        throw new NotImplementedException();
+        try
+        {
+            var returnResponse = new List<Pet>();
+            using var client = new HttpClient();
+            string url = $"{_baseUrl}api/petController";
+            HttpResponseMessage apiResponse = await client.GetAsync(url);
+
+            if (apiResponse.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                string response = await apiResponse.Content.ReadAsStringAsync();
+                var deserilizeResponse = JsonConvert.DeserializeObject<MainResponseModel>(response);
+
+                if (deserilizeResponse.IsSuccess)
+                {
+                    returnResponse = JsonConvert.DeserializeObject<List<Pet>>(deserilizeResponse.Content.ToString());
+                }
+            }
+            return returnResponse;
+        }
+        catch (Exception)
+        {
+            return new List<Pet>();
+        }
     }
 }
